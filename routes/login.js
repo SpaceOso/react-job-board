2fde48095d9bf20b62668abd34950024027971c1
Miguel Rico <miguelricodev@gmail.com>
2017-06-08 07:13:12 -0400

    moving to laptop

--- routes/login.js
+++ routes/login.js
@@ -12,8 +12,7 @@ var Applicants = require('../models/applicants');
 router.post('/', function (req, res, next) {
     console.log("inside the root login path with req:");
     console.log(req.body);
-    console.log("and respond:");
-    console.log(res);
+   User.query()
     res.status(201).json({
         message: 'user logged in!',
     });
