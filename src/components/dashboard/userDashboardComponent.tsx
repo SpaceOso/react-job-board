1c3899ebd4d9149043eb2c54a06c5af52885d7ec
Miguel Rico <miguelricodev@gmail.com>
2017-07-22 11:20:57 -0400

    workgin on user dashboard

--- src/components/dashboard/userDashboardComponent.tsx
+++ src/components/dashboard/userDashboardComponent.tsx
@@ -11,13 +11,23 @@ import UserDashboardNavMenu from "./nav-menu/userDashboardNavMenu";
 
 
 //redux
-interface Props extends RouteComponentProps<any>{
+interface Props extends RouteComponentProps<any> {
 	user: User,
 	employer: Employer,
-	fetchThisUserInfo: (userId)=>{}
-	saveJobPost: (jobInfo, userId)=>{}
-	submitEmployerRegistration:(userData)=>{}
+	fetchThisUserInfo: (userId) => {}
+	saveJobPost: (jobInfo, userId) => {}
+	submitEmployerRegistration: (userData) => {}
 }
+
+/*What is the purpose of this component?
+ * Needs to check if the user has a registered employer:
+ *   If it does not send us to the register component
+ *   If it does send us to the home component
+ *How do we check if there is a registered user?
+ *   We check this.props.user.employerId
+ *   If undefined we send the user to the register component. Else we send them home.
+ *Why does it error out sometimes with null user?
+ *   I think we need to redirect user out of here first then null out the user in the state.*/
 
 class UserDashboardComponent extends React.Component<Props> {
 	constructor(props) {
@@ -27,26 +37,20 @@ class UserDashboardComponent extends React.Component<Props> {
 		this.handleEmployerRegistration = this.handleEmployerRegistration.bind(this);
 		this.checkForEmployer = this.checkForEmployer.bind(this);
 		this.submitJobPost = this.submitJobPost.bind(this);
-		this.waitForLoad = this.waitForLoad.bind(this);
+		this.createSwitchRoutes = this.createSwitchRoutes.bind(this);
 
 		// this.fetchEmployerInfo();
-	}
-
-	componentDidMount() {
-		// this.fetchEmployerInfo();
-		// this.checkForLogInErrors();
-		// this.checkForEmployer();
 	}
 
 	checkForLogInErrors() {
 		console.log("checkForLogInErrors:", this.props.user._id === undefined);
-		if(this.props.user._id === undefined){
+		if (this.props.user._id === undefined) {
 			console.log("we should be re-routing to the login page then...", this.props.match);
 		}
-		return this.props.user._id === undefined ?  <Redirect to={'/login'} /> : null;
+		return this.props.user._id === undefined ? <Redirect to={'/login'}/> : null;
 	}
 
-	handleEmployerRegistration(employerData){
+	handleEmployerRegistration(employerData) {
 		console.log("Trying to register employer with the following inof:", employerData);
 		let userData = {employerData, userId: this.props.user._id};
 		this.props.submitEmployerRegistration(userData);
@@ -56,7 +60,7 @@ class UserDashboardComponent extends React.Component<Props> {
 	 * If it does not we will display the employer registration component.
 	 * Otherwise we weill load up the main layout*/
 
-	checkForEmployer(){
+	checkForEmployer() {
 		console.log("checkForEmployer:", this.props.user.employerId);
 		console.log("check path wth..", `${this.props.location.pathname}/home`);
 		return this.props.user.employerId === null ? <Redirect to={`${this.props.location.pathname}/register`}/>
@@ -64,70 +68,14 @@ class UserDashboardComponent extends React.Component<Props> {
 	}
 
 	/* This will handle sending the job post information to the back end.*/
-	submitJobPost(jobPost){
+	submitJobPost(jobPost) {
 		this.props.saveJobPost(jobPost, this.props.user._id);
 		console.log("we are saving a job with this info:", jobPost);
 	}
 
-	/*Will show spinner as we gather the user information for the dashboard*/
-	waitForLoad(){
-		// if(this.props.user === null || this.props.user === undefined){
-		if(this.props.user.isFetching){
-			return <SpinnerComponent/>
-		} else {
-			console.log("inside the dashboard component with route:", this.props.match.params);
-			return (
-				<Switch>
-
-					{/*REGISTER COMPONENT*/}
-					<Route path={`${this.props.match.path}/register`}
-					       render={() => {
-						       return (<CompRegisterComponent
-							       submitData={this.handleEmployerRegistration}
-						       />)}
-					       }
-					/>
-					{/*CREATE JOB COMPONENT*/}
-					<Route path={`${this.props.match.path}/createjob`}
-					       render={props => (
-						       <CreateJobComponent
-							       userId={this.props.user._id}
-							       employer={this.props.user.employerId}
-							       submitJobPost={this.submitJobPost}
-							       {...props}
-						       />
-					       )}
-					/>
-					{/*EDIT POSTINGS COMPONENT*/}
-					<Route path={`${this.props.match.path}/editpostings`}
-					       render={props =>(
-						       <EditJobsComponent
-							       employer={this.props.employer}
-							       jobs={this.props.employer.jobs}
-						       />
-					       )}
-					/>
-					{/*APPLICANT LIST COMPONENT*/}
-					<Route path={`${this.props.match.path}/home`}
-					       render={props => (
-						       <ApplicantListComponent
-							       user={this.props.user}
-							       employer={this.props.employer}
-						       />
-					       )}
-					/>
-				</Switch>
-			)
-		}
-	}
-
-	render() {
-		console.log("DASHBOARD RENDER IS BEING CALLED");
-
+	createSwitchRoutes() {
 		return (
 			<div className="jb-dashboard">
-				{this.checkForLogInErrors()}
-				{this.checkForEmployer()}
 				{/*NAV MENU*/}
 				<Route path={`${this.props.match.path}`}
 				       render={props => (
@@ -137,11 +85,63 @@ class UserDashboardComponent extends React.Component<Props> {
 				       }
 				/>
 				<div className="layout-container">
-					{/*Removed switch component form here*/}
-					{this.waitForLoad()}
+					<Switch>
+						{/*REGISTER COMPONENT*/}
+						<Route path={`${this.props.match.path}/register`}
+						       render={() => {
+							       return (<CompRegisterComponent
+								       submitData={this.handleEmployerRegistration}
+							       />)
+						       }
+						       }
+						/>
+						{/*CREATE JOB COMPONENT*/}
+						<Route path={`${this.props.match.path}/createjob`}
+						       render={props => (
+							       <CreateJobComponent
+								       userId={this.props.user._id}
+								       employer={this.props.user.employerId}
+								       submitJobPost={this.submitJobPost}
+								       {...props}
+							       />
+						       )}
+						/>
+						{/*EDIT POSTINGS COMPONENT*/}
+						<Route path={`${this.props.match.path}/editpostings`}
+						       render={props => (
+							       <EditJobsComponent
+								       employer={this.props.employer}
+								       jobs={this.props.employer.jobs}
+							       />
+						       )}
+						/>
+						{/*APPLICANT LIST COMPONENT*/}
+						<Route path={`${this.props.match.path}/home`}
+						       render={props => (
+							       <ApplicantListComponent
+								       user={this.props.user}
+								       employer={this.props.employer}
+							       />
+						       )}
+						/>
+					</Switch>
 				</div>
 			</div>
 		)
+	}
+
+	render() {
+
+		if (this.props.user.isFetching) {
+			return <SpinnerComponent/>
+		}
+
+		if(this.props.user.employerId === undefined){
+			this.checkForLogInErrors();
+			this.checkForEmployer();
+		}
+
+		return this.createSwitchRoutes();
 	}
 };
 
