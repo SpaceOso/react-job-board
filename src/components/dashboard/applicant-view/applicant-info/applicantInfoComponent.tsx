a416a73fb5df33ed5ec9fcab60a619fff046f9e0
Miguel Rico <miguelricodev@gmail.com>
2017-10-22 19:55:34 -0400

    added website, likedin and github properties to Applicants interface, working on creating the applicant info component

--- src/components/dashboard/applicant-view/applicant-info/applicantInfoComponent.tsx
+++ src/components/dashboard/applicant-view/applicant-info/applicantInfoComponent.tsx
@@ -8,6 +8,22 @@ interface MyProps{
 class ApplicantInfoComponent extends React.Component<MyProps>{
 	constructor(props){
 		super(props);
+
+		this.createSocialLinks = this.createSocialLinks.bind(this);
+	}
+
+
+	createSocialLinks(){
+		let applicantLinks: string[] = [];
+		applicantLinks.push(this.props.applicant.website !== null ? this.props.applicant.website : '');
+		applicantLinks.push(this.props.applicant.linkedin !== null ? this.props.applicant.linkedin : '');
+		applicantLinks.push(this.props.applicant.github !== null ? this.props.applicant.github : '');
+
+		const socialLinks = applicantLinks.map((link, key) =>{
+			return <li key={key}>{link}</li>
+		});
+
+		return <ul>{socialLinks}</ul>
 	}
 
 	render(){
@@ -24,6 +40,7 @@ class ApplicantInfoComponent extends React.Component<MyProps>{
 					<p>{applicantAddress}</p>
 					<p>{applicantPhone}</p>
 					<p>{applicantEmail}</p>
+					{this.createSocialLinks()}
 				</div>
 			</div>
 		)
