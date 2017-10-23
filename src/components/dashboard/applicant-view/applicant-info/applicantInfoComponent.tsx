8fe42b58f88300f70bda40975481c4a7e91e7d2c
WebDevMR <miguelricodev@gmail.com>
2017-10-23 07:17:31 -0400

    added new github icon, working on refactoring createSocialLinks

--- src/components/dashboard/applicant-view/applicant-info/applicantInfoComponent.tsx
+++ src/components/dashboard/applicant-view/applicant-info/applicantInfoComponent.tsx
@@ -1,38 +1,47 @@
 import * as React from 'react';
 import {Applicants} from "../../../../types/index";
 
-interface MyProps{
+interface MyProps {
 	applicant: Applicants
 }
 
-class ApplicantInfoComponent extends React.Component<MyProps>{
-	constructor(props){
+class ApplicantInfoComponent extends React.Component<MyProps> {
+	constructor(props) {
 		super(props);
 
 		this.createSocialLinks = this.createSocialLinks.bind(this);
 	}
 
 
-	createSocialLinks(){
-		let applicantLinks: string[] = [];
+	createSocialLinks() {
+		let applicantLinks: any[] = [];
+		const socialLinksites = [{link:'github', icon: 'icon-github'}, 'linkedin', 'website'];
+
+		socialLinksites.map((social, key) => {
+			let link = social.link.toString();
+			if(this.props.applicant[link] !== null){
+				// applicantLinks.push()
+			}
+		});
+
 		applicantLinks.push(this.props.applicant.website !== null ? this.props.applicant.website : '');
 		applicantLinks.push(this.props.applicant.linkedin !== null ? this.props.applicant.linkedin : '');
 		applicantLinks.push(this.props.applicant.github !== null ? this.props.applicant.github : '');
 
-		const socialLinks = applicantLinks.map((link, key) =>{
-			return <li key={key}>{link}</li>
+		const socialLinks = applicantLinks.map((social, key) => {
+			return <li key={key}>{social.link}</li>
 		});
 
 		return <ul>{socialLinks}</ul>
 	}
 
-	render(){
-		let applicant = this.props.applicant;
-		let applicantAddress = `${applicant.city}, ${applicant.state}, ${applicant.zip}`;
-		let applicantEmail = this.props.applicant.email;
-		let applicantPhone = this.props.applicant.cellPhone;
+	render() {
+		const applicant = this.props.applicant;
+		const applicantAddress = `${applicant.city}, ${applicant.state}, ${applicant.zip}`;
+		const applicantEmail = this.props.applicant.email;
+		const applicantPhone = this.props.applicant.cellPhone;
 
-		return(
+		return (
 			<div>
 				I'm the ApplicantInfoComponent
 				<div>
