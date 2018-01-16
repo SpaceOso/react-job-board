cc5fc651dce73a1692f56309be030cee4eff028d
Miguel Rico <miguelricodev@gmail.com>
2018-01-16 05:12:54 -0500

    added different development settigns for webpack

--- webpack.common.js
+++ webpack.common.js
@@ -2,39 +2,32 @@ const {resolve} = require('path');
 const webpack = require('webpack');
 const DotenvPlugin = require('webpack-dotenv-plugin');
 
-module.exports = env => ({
-	// context: resolve('src'),
-	entry: "./src/index.tsx",
-	output: {
-		path: resolve('public'),
-		filename: "bundle.js",
-		publicPath: 'public/',
-	},
-    devtool: "source-map",
+module.exports = {
+    // context: resolve('src'),
+    entry: "./src/index.tsx",
+    output: {
+        path: resolve('public'),
+        filename: "bundle.js",
+        publicPath: 'public/',
+    },
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".jsx"]
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify(env.dev)
-            },
-        }),
-        new webpack.optimize.UglifyJsPlugin(),
-		new DotenvPlugin({
-			sample: './.env.hide',
-			path: './.env',
-			safe: true
-		})
+        new DotenvPlugin({
+            sample: './.env.hide',
+            path: './.env',
+            safe: true
+        })
     ],
     module: {
-		loaders: [
+        loaders: [
             { test: /\.([tj])sx?$/, exclude: /node_modules/, use: { loader: 'awesome-typescript-loader' } },
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-			{
-				test: /\.scss$/,
-				loaders: ["style-loader", "css-loader", "sass-loader"]
-			},
+            {
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            },
             {
                 test: /\.(png|jpg|gif|svg)$/,
                 use: [
@@ -47,11 +40,11 @@ module.exports = env => ({
                     }
                 ]
             }
-		]
-	},
-	node: {
-		dns: 'empty',
-		net: 'empty',
-		fs:'empty'
-	}
-});
+        ]
+    },
+    node: {
+        dns: 'empty',
+        net: 'empty',
+        fs:'empty'
+    }
+};
